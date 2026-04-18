44ae171b7620da803a2e16ad8f84f7f3bcb89bc9
cjsemicolon <cjsemicolon@gmail.com>
2026-04-18 12:24:35 +0100

    assignments

---
+++ Chapter 3/medicaldiagnosis.py
@@ -0,0 +1,46 @@
+"""
+(Turing Test) The great British mathematician Alan Turing proposed a simple test
+to determine whether machines could exhibit intelligent behavior. A user sits at a comput-
+er and does the same text chat with a human sitting at a computer and a computer oper-
+ating by itself. The user doesn’t know if the responses are coming back from the human
+or the independent computer. If the user can’t distinguish which responses are coming
+from the human and which are coming from the computer, then it’s reasonable to say that
+the computer is exhibiting intelligence.
+Create a script that plays the part of the independent computer, giving its user a sim-
+ple medical diagnosis. The script should prompt the user with 'What is your problem?'
+When the user answers and presses Enter, the script should simply ignore the user’s input,
+then prompt the user again with 'Have you had this problem before (yes or no)?' If
+the user enters 'yes', print 'Well, you have it again.' If the user answers 'no', print
+'Well, you have it now.'
+Would this conversation convince the user that the entity at the other end exhibited
+intelligent behavior? Why or why not?
+
+psuedo code
+collect input from the user
+print response and collect input again
+if user_response == yes print you have it again
+if user_response == no print you have it now
+
+"""
+
+user_problem = input("What is your problem?: ")
+user_history = input("Have you had this problem before? (Yes or No): ").lower()
+
+if user_history == "yes":
+    print("Well, you have it again")
+
+elif user_history == "no":
+    print("Well, you have it now")
+
+
+"""
+yes it would, because it assed a question, got answer and seemingly asked a follow up question which it then responded to accordingly.
+"""
+
+
+
+
+
+
+
+
