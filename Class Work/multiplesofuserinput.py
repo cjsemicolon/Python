eb3110faefe85020fc45dcee51d5be219869e552
cjsemicolon <cjsemicolon@gmail.com>
2026-04-20 10:36:41 +0100

    userinput multiplication table

---
+++ Class Work/multiplesofuserinput.py
@@ -0,0 +1,15 @@
+"""
+Psuedocode
+collect user input
+create a loop that runs from 1-10
+increase the count by 1
+multiply the count by the user input
+print the result
+"""
+
+user_input = 0
+user_input = int(input("enter a number: "))
+for number in range(1,11):
+    print (user_input, "x", number, "=", user_input * number)
+    
+
